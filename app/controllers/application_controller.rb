8726403a20d752da719f31358fd933e3cd2bbeeb
taiyo2001 <taiyo2778@gmail.com>
2024-01-25 20:41:59 +0900

    feat: devise init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
+
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    # TODO: 共通処理になってしまうのでdevise_controllerでオーバーライトさせたい
+    if devise_controller?
+      'authentication'
+    else
+      'application'
+    end
+  end
 end
