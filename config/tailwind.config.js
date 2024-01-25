21d2b86a2ebeb3436ab470784ae19b51511d14c3
taiyo2001 <taiyo2778@gmail.com>
2024-01-26 04:47:18 +0900

    feat: users base

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -13,8 +13,14 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
       colors: {
+        'custom-blue': {
+          100: '#2C6FA2'
+        },
         'custom-green': '#55C500',
         'custom-gray': '#EDEEEE',
+      },
+      height: {
+        'screen-minus-header': 'calc(100vh - 100px)',
       },
     },
   },
