01ed3a42cb08ffc3cd05be1e74544ad70d809517
taiyo2001 <taiyo2778@gmail.com>
2024-01-26 04:56:41 +0900

    fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,28 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'custom-blue': {
+          100: '#2C6FA2'
+        },
+        'custom-green': '#55C500',
+        'custom-gray': '#EDEEEE',
+      },
+      height:   {
+        'screen-minus-header': 'calc(100vh - 100px)',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
   ]
 }
